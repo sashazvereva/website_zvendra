0a93443f88e5a63efedc8e7c6bd50aeda001540c
sashazvereva <59964462+sashazvereva@users.noreply.github.com>
2021-11-17 23:13:01 +0300

    /

--- script.js
+++ script.js
@@ -12,36 +12,6 @@ $(document).ready(function () {
 });
 
 
-/* Smooth slide navigation */
-const menuLinks = document.querySelectorAll('a[data-goto]');
-if(menuLinks.length > 0) {
-    menuLinks.forEach(menuLink => {
-        menuLink.addEventListener("click", onMenuLinkClick);
-    });
-
-    
-    function onMenuLinkClick(e) {
-        const menuLink = e.target;
-        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-            const gotoBlock = document.querySelector(menuLink.dataset.goto);
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
-
-            if (iconMenu.classList.contains('_.active')) {
-                document.body.classList.remove('_lock');
-                iconMenu.classList.remove('_active');
-                menuBody.classList.remove('_active');
-            }
-
-            window.scrollTo({
-                top:gotoBlockValue,
-                behavior: "smooth"
-            });
-            e.preventDefault();
-        }
-    }
-}
-
-
 /* Button */
 let animateButton = function(e) {
 
@@ -140,4 +110,34 @@ if (iconMenu) {
         iconMenu.classList.toggle('_active');
         menuBody.classList.toggle('_active');
     });
+}
+
+
+/* Smooth slide navigation */
+const menuLinks = document.querySelectorAll('a[data-goto]');
+if(menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+            if (iconMenu.classList.contains('_.active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
+            window.scrollTo({
+                top:gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
 }
