2e74880e12eb2356076d0b933a48ce7420dd3d8d
sashazvereva <59964462+sashazvereva@users.noreply.github.com>
2021-11-17 17:13:09 +0300

    .

--- script.js
+++ script.js
@@ -18,11 +18,20 @@ if(menuLinks.length > 0) {
     menuLinks.forEach(menuLink => {
         menuLink.addEventListener("click", onMenuLinkClick);
     });
+
+    
     function onMenuLinkClick(e) {
         const menuLink = e.target;
         if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
             const gotoBlock = document.querySelector(menuLink.dataset.goto);
             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+            if (iconMenu.classList.contains('_.active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
             window.scrollTo({
                 top:gotoBlockValue,
                 behavior: "smooth"
@@ -120,4 +129,15 @@ if (isMobile.any()) {
 
     } else {
     document.body.classList.add ('_pc');
+}
+
+const iconMenu = document.querySelector('menu_icon');
+const menuBody = document.querySelector('.menu_body');
+
+if (iconMenu) {
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
 }
